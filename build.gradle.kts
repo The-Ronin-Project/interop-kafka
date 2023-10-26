e805dc3837d6d14c01d93e9441cebb260cb6b1ae
Josh Smith <43484632+arssycro@users.noreply.github.com>
2023-10-26 14:26:25 -0500

    INT-2309: Move to SonarQube and single unit test GHA (#49)

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     alias(libs.plugins.interop.gradle.integration)
     alias(libs.plugins.interop.gradle.publish)
     alias(libs.plugins.interop.version.catalog)
+    alias(libs.plugins.interop.gradle.sonarqube)
 }
 
 subprojects {
--- interop-kafka-testing-client/interop-kafka-testing-client.gradle.kts
+++ interop-kafka-testing-client/interop-kafka-testing-client.gradle.kts
@@ -5,3 +5,7 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2") // replace 1.5.2 with the latest version
     implementation(libs.interop.fhir)
 }
+
+sonar {
+    isSkipProject = true
+}
